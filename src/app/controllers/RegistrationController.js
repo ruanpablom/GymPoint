55ce5dbfd8e33b919e474f372c38ef3de5d4bb9a
Ruan Pablo Medeiros <pm.ruan@gmail.com>
2019-10-23 12:28:31 -0300

    finalizado gestão de matriculas

--- src/app/controllers/RegistrationController.js
+++ src/app/controllers/RegistrationController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { addMonths, parseISO } from 'date-fns';
+import { addMonths, parseISO, isPast } from 'date-fns';
 
 import Queue from '../../lib/Queue';
 import RegistrationMail from '../jobs/RegistrationMail';
@@ -9,6 +9,30 @@ import Student from '../models/Student';
 import Registration from '../models/Registration';
 
 class RegistrationController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    const registrations = await Registration.findAll({
+      order: ['start_date'],
+      attributes: ['id', 'start_date', 'end_date', 'price'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: Student,
+          as: 'student',
+          attributes: ['id', 'name', 'email', 'age', 'weight', 'height'],
+        },
+        {
+          model: Plan,
+          as: 'plan',
+          attributes: ['id', 'title', 'duration', 'price'],
+        },
+      ],
+    });
+
+    return res.json(registrations);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       start_date: Yup.date().required(),
@@ -21,6 +45,10 @@ class RegistrationController {
     }
 
     const { start_date, plan_id, student_id } = req.body;
+
+    if (isPast(parseISO(start_date))) {
+      return res.status(400).json({ error: 'Start date is on the past' });
+    }
 
     const plan = await Plan.findByPk(plan_id);
 
@@ -55,6 +83,72 @@ class RegistrationController {
     });
 
     return res.json(registration);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      start_date: Yup.date(),
+      plan_id: Yup.number(),
+      student_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails' });
+    }
+
+    const registration = await Registration.findByPk(req.params.id);
+
+    const { plan_id, student_id, start_date } = req.body;
+
+    const plan = await Plan.findByPk(plan_id || registration.plan_id);
+
+    const student = await Student.findByPk(
+      student_id || registration.student_id
+    );
+
+    if (!registration) {
+      res.status(400).json({ error: 'Registration not found' });
+    }
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan does not exists' });
+    }
+
+    if (!student) {
+      return res.status(400).json({ error: 'Student does not exists' });
+    }
+
+    if (start_date) {
+      if (isPast(parseISO(start_date))) {
+        return res.status(400).json({ error: 'Start date is on the past' });
+      }
+
+      registration.start_date = parseISO(start_date);
+    }
+
+    registration.plan_id = plan_id || registration.plan_id;
+
+    registration.student_id = student_id || registration.student_id;
+
+    if (plan_id || start_date) {
+      registration.end_date = addMonths(parseISO(start_date), plan.duration);
+      registration.price = plan.price * plan.duration;
+    }
+
+    await registration.save();
+
+    return res.json(registration);
+  }
+
+  async delete(req, res) {
+    const registration = await Registration.destroy({
+      where: { id: req.params.id },
+    });
+    if (!registration) {
+      return res.status(400).json({ error: 'Registration not found' });
+    }
+
+    return res.json({ success: 'Registration deleted' });
   }
 }
 
